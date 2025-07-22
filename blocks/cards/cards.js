3d50fa5da9999c8ac43bd72c48c6f6816c3b4055
Sean <sean.ohern@ey.com>
2025-07-22 13:09:05 -0400

    remove generic dom parsing

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,3 +1,5 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 import {
   registerAlpineComponent,
   markForLazyLoad,
@@ -7,11 +9,6 @@ import {
   loadTemplate,
   getTemplatePath,
 } from '../../scripts/block-utils.js';
-import {
-  parseBlockContent,
-  isImageOnly,
-  getOptimizedImage,
-} from '../../scripts/block-dom-parser.js';
 import { IMAGE_WIDTHS } from '../../scripts/constants.js';
 
 /**
@@ -23,23 +20,36 @@ export default async function decorate(block) {
   // Generate a unique ID for the block
   const blockId = `cards${Math.random().toString(36).substring(2, 10)}`;
 
-  // Process cards data using generic parser
-  const cardsData = parseBlockContent(block, {
-    rowParser: (row) => {
-      const card = { image: null, body: null };
+  // Process cards data
+  const cardsData = [];
+  [...block.children].forEach((row) => {
+    const card = { image: null, body: null };
 
-      [...row.children].forEach((div) => {
-        if (isImageOnly(div)) {
-          const optimizedPic = getOptimizedImage(div, {
-            width: IMAGE_WIDTHS.DEFAULT,
-          });
-          card.image = optimizedPic.outerHTML;
-        } else {
-          card.body = div.innerHTML;
-        }
-      });
-      return card;
-    },
+    // Process each column in the row
+    [...row.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        // Process image
+        const img = div.querySelector('img');
+        const optimizedPic = createOptimizedPicture(
+          img.src,
+          img.alt,
+          false,
+          [{ width: IMAGE_WIDTHS.DEFAULT }],
+        );
+        moveInstrumentation(img, optimizedPic.querySelector('img'));
+
+        /* Can directly set outer/innerHTML because source is trusted
+        because it is CMS content
+        Create a wrapper and append or DOMPurify for third-party content
+        Store as DOM element */
+        card.image = optimizedPic.outerHTML;
+      } else {
+        // Store content as HTML
+        card.body = div.innerHTML;
+      }
+    });
+
+    cardsData.push(card);
   });
 
   // Create unique component name for use with Alpine.js
