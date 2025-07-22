5495dfe8c1aefc07b1c9c04574110075bed6ee9e
Sean <sean.ohern@ey.com>
2025-07-22 12:58:07 -0400

    Updated cards css and added code to handle alpine component independence

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,24 +1,64 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
+import {
+  registerAlpineComponent,
+  markForLazyLoad,
+} from '../../scripts/alpine-loader.js';
+import {
+  appendBlockContent,
+  loadTemplate,
+  getTemplatePath,
+} from '../../scripts/block-utils.js';
+import {
+  parseBlockContent,
+  isImageOnly,
+  getOptimizedImage,
+} from '../../scripts/block-dom-parser.js';
+import { IMAGE_WIDTHS } from '../../scripts/constants.js';
 
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+/**
+ * Main block function
+ * @param {HTMLElement} block The block element
+ * @returns {Promise<HTMLElement>} The decorated block
+ */
+export default async function decorate(block) {
+  // Generate a unique ID for the block
+  const blockId = `cards${Math.random().toString(36).substring(2, 10)}`;
+
+  // Process cards data using generic parser
+  const cardsData = parseBlockContent(block, {
+    rowParser: (row) => {
+      const card = { image: null, body: null };
+
+      [...row.children].forEach((div) => {
+        if (isImageOnly(div)) {
+          const optimizedPic = getOptimizedImage(div, {
+            width: IMAGE_WIDTHS.DEFAULT,
+          });
+          card.image = optimizedPic.outerHTML;
+        } else {
+          card.body = div.innerHTML;
+        }
+      });
+      return card;
+    },
   });
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
-  });
-  block.textContent = '';
-  block.append(ul);
+
+  // Create unique component name for use with Alpine.js
+  // Allows multiple instances of the component on
+  // the same page without conflicts
+  const componentName = `cardsComponent${blockId}`;
+
+  registerAlpineComponent(componentName, () => ({
+    cards: cardsData,
+  }));
+
+  // Get template path
+  const templatePath = getTemplatePath('cards.html', import.meta);
+
+  // Load template and create container
+  const container = await loadTemplate(templatePath);
+
+  // Mark for lazy loading
+  markForLazyLoad(container, componentName);
+
+  return appendBlockContent(block, container);
 }
